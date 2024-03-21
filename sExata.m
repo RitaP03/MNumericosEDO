bd018a49eee0c30d0265456520e874eadecc8cf6
Wisura <joao9.claro99@gmail.com>
2024-03-21 12:42:45 +0000

    Add input fields on livescript

--- sExata.m
+++ sExata.m
@@ -1,7 +1,8 @@
 function [t, y] = sExata(f,a,b,n,y0)
-sExata=dsolve(['Dy=', f], ['y(',num2str(a),')=',num2str(y0)]);
-% sExata=dsolve(diff(f), ['y(',num2str(a),')=',num2str(y0)]);
-g=@(t) eval(vectorize(char(sExata)));
 h=(b-a)/n;
 t=a:h:b;
+syms y(x)
+yExata = dsolve(diff(y,x)==f(x,y), ['y(',num2str(a),')=',num2str(y0)]);
+g=@(x) eval(vectorize(char(yExata)));
 y=g(t);
+end
